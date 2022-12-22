f592e42cfb14ae56ac7f6251c5906b04ce7ddb9a
Kalrul <prizsabertler@gmail.com>
2022-12-22 17:09:52 +0300

    new era

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,20 +1,12 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  let body;
-  let firstName;
-  let lastName;
-
-  return uploadPhoto()
-    .then((data) => {
-      body = data.body;
-      createUser()
-        .then((data) => {
-          firstName = data.firstName;
-          lastName = data.lastName;
-          console.log(`${body} ${firstName} ${lastName}`);
-        })
-        .catch(() => console.log('Signup system offline'));
-      })
-      .catch(() => console.log('Signup system offline'));
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((values) => {
+      const { body } = values[0];
+      const { firstName } = values[1];
+      const { lastName } = values[1];
+      console.log(`${body} ${firstName} ${lastName}`);
+    })
+    .catch(() => console.log('Signup system offline'));
 }
