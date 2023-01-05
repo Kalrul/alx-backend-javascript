55dbcd070d7b67070894e063f33477b48f1376f7
Kalrul <prizsabertler@gmail.com>
2023-01-05 17:35:01 +0300

    babuska

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,12 +1,19 @@
-export default function updateStudentGradeByCity(students, city, newGrades) {
-    return students.filter((student) => student.location === city).map((student) => {
-      const newStudents = student;
-      newStudents.grade = 'N/A';
-      for (const grade of newGrades) {
-        if (newStudents.id === grade.studentId) {
-          newStudents.grade = grade.grade;
-        }
-      }
-      return newStudents;
-    });
-  }
+/* eslint-disable */
+function updateStudentGradeByCity(students, city, newGrades) {
+  const studentsInCity = students.filter((student) => student.location === city);
+  return studentsInCity.map((student) => {
+    const filteredGrades = newGrades.filter((grade) => grade.studentId === student.id);
+    if (filteredGrades.length > 0) {
+      return {
+        ...student,
+        grade: filteredGrades[0].grade,
+      };
+    }
+    return {
+      ...student,
+      grade: 'N/A',
+    };
+  });
+}
+
+export default updateStudentGradeByCity;
